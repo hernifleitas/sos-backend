12ad7a8b5fd475b9f1951f7935678eb4f28459f9
Hernan <hernifleitas235@gmail.com>
2025-11-20 21:50:59 -0300

    OTRA VE

--- api/zonas-peligrosas.js
+++ api/zonas-peligrosas.js
@@ -1,3 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
 router.get('/', (req, res) => {
   const zonas = [
     // Zona peligrosa
@@ -29,3 +32,5 @@ router.get('/', (req, res) => {
   ];
   res.json({ success: true, zonas });
 });
+
+module.exports = router;
