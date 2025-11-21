4efb9faebcb8602c928dbc3a2b7f744427047ea9
Hernan <hernifleitas235@gmail.com>
2025-11-20 23:24:05 -0300

    P

--- api/zonas-peligrosas.js
+++ api/zonas-peligrosas.js
@@ -5,7 +5,7 @@ router.get('/', (req, res) => {
   const zonas = [
     // Zona peligrosa
     {
-      id: 2,
+      id: 1,
       nombre: 'Zona peligrosa',
       descripcion: 'Zona con reportes de robos frecuentes.',
       tipo: 'peligrosa',
@@ -19,7 +19,7 @@ coordenadas: [
 
     // Zona peligrosa
     {
-      id: 1,
+      id: 2,
       nombre: 'Zona peligrosa',
       descripcion: 'Zona con reportes de robos frecuentes.',
       tipo: 'peligrosa',
@@ -30,9 +30,23 @@ coordenadas: [
   [-34.82640, -58.45955]
 ]
     },
-    // Local No devuelve efectivo - Mr Tasty Montegrande
+
+     // Zona peligrosa
     {
       id: 3,
+      nombre: 'Zona peligrosa',
+      descripcion: 'Zona con reportes de robos frecuentes.',
+      tipo: 'peligrosa',
+coordenadas: [
+  [-34.80980, -58.43715],  // Noroeste
+  [-34.80980, -58.43676],  // Noreste
+  [-34.81025, -58.43676],  // Sureste
+  [-34.81025, -58.43715]   // Suroeste
+]
+    },
+    // Local No devuelve efectivo - Mr Tasty Montegrande
+    {
+      id: 4,
       nombre: 'Mr Tasty',
       descripcion: 'Local que no devuelve efectivo a repartidores',
       tipo: 'sin-efectivo',
@@ -42,8 +56,61 @@ coordenadas: [
         [-34.81520, -58.46885],
         [-34.81520, -58.46895]
       ]
-    }
+    },
+
+    // Zona peligrosa
+    {
+      id: 5,
+      nombre: 'Zona peligrosa',
+      descripcion: 'Zona con reportes de robos frecuentes.',
+      tipo: 'peligrosa',
+coordenadas: [
+  [-34.80850, -58.42745],  // Noroeste
+  [-34.80850, -58.42680],  // Noreste
+  [-34.80795, -58.42680],  // Sureste
+  [-34.80795, -58.42745]   // Suroeste
+]
+    },
+
+    // Zona peligrosa
+    {
+      id: 6,
+      nombre: 'Zona peligrosa',
+      descripcion: 'Zona con reportes de robos frecuentes.',
+      tipo: 'peligrosa',
+coordenadas: [
+  [-34.80463, -58.43936],
+  [-34.80478, -58.43889],
+  [-34.80510, -58.43870],
+  [-34.80545, -58.43890],
+  [-34.80562, -58.43936],
+  [-34.80545, -58.43982],
+  [-34.80510, -58.44002],
+  [-34.80478, -58.43983]
+]
+    },
+
+    // Zona peligrosa
+    {
+      id: 7,
+      nombre: 'Zona peligrosa',
+      descripcion: 'Zona con reportes de robos frecuentes.',
+      tipo: 'peligrosa',
+coordenadas: [
+  [-34.82863, -58.44033],  // Noroeste
+  [-34.82863, -58.43935],  // Norte
+  [-34.82913, -58.43885],  // Noreste
+  [-34.82963, -58.43935],  // Este
+  [-34.82963, -58.44033],  // Sureste
+  [-34.82913, -58.44083],  // Sur
+  [-34.82863, -58.44033],  // Suroeste
+  [-34.82863, -58.43935]   // Oeste
+]
+    },
+
   ];
+
+
   res.json({ success: true, zonas });
 });
 
